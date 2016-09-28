280376de82d5c9171bae49221946e1ded5e563e7
Jonathan Engelsma <jengelsma@gmail.com>
2016-09-28 12:56:46 -0400

    Initial Commit

---
+++ LoginViaSafariDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginViaSafariDemo
+//
+//  Created by Jonathan Engelsma on 9/28/16.
+//  Copyright © 2016 Jonathan Engelsma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
